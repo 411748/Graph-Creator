a8c30b76b319a46fba9eec78f6ba9223c25c876b
411748 <411748@bsd48.org>
2024-05-26 16:33:02 -0700

    comments

--- main.cpp
+++ main.cpp
@@ -1,29 +1,36 @@
+//Nick Braun
+//5/26/2024
+//Graph Creator - functions to add vertexs edges, remove them, print, path, and quit
+//inclues
 #include <iostream>
 #include <cstring>
 using namespace std;
 
+
+//builing the class for graph
 class Graph {
 public:
+  //building objects
   int adjMatrix[21][21];
   int numVertices;
-
+  
   Graph() {
     numVertices = 1;
     memset(adjMatrix, 0, sizeof(adjMatrix));
     adjMatrix[0][0] = 32; 
   }
-
+  //function to add a vertex to graph
   void addVertex(int label) {
-    if (adjMatrix[0][0] == 0) {
-      adjMatrix[0][0] = label; 
+    if (adjMatrix[0][0] == 0) { //empty
+      adjMatrix[0][0] = label; //build
     } else {
       for (int i = 0; i <= numVertices; i++) {
         for (int j = 0; j <= numVertices; j++) {
           if (adjMatrix[i][j] == 0) {
             if (i == 0 || j == 0) {
-              adjMatrix[i][j] = label;
+              adjMatrix[i][j] = label; //build
             } else {
-              adjMatrix[i][j] = 0;
+              adjMatrix[i][j] = 0; 
             }
           }
         }
@@ -31,21 +38,22 @@ public:
     }
     numVertices++;
   }
-
+  //func to remove vertexs
   void removeVertex(char label) {
     int index = -1;
     for (int i = 0; i <= numVertices; i++) {
-      if (adjMatrix[i][0] == label) {
+      if (adjMatrix[i][0] == label) { //finding in graph
         index = i;
         break;
       }
     }
-    if (index == -1) return;
+    if (index == -1) return; //problem finding
 
     for (int i = 0; i <= numVertices; i++) {
       for (int j = 0; j <= numVertices; j++) {
-        if (i >= index || j >= index) {
-          if (i == j) {
+        if (i >= index || j >= index) { //finding
+	  //shifting
+          if (i == j) { 
             adjMatrix[i][j] = 0;
           } else if (i <= 1) {
             adjMatrix[i][j] = adjMatrix[i][j+1];
@@ -61,26 +69,26 @@ public:
     numVertices--;
   }
 
-  void addEdge(char from, char to, int weight) {
+  void addEdge(char from, char to, int weight) { //func to add edge
     for (int i = 0; i <= numVertices; i++) {
       for (int j = 0; j <= numVertices; j++) {
         if (adjMatrix[i][0] == from && adjMatrix[j][0] == to) {
-          adjMatrix[i][j] = weight;
+          adjMatrix[i][j] = weight; //building
         }
       }
     }
   }
 
-  void removeEdge(char from, char to) {
+  void removeEdge(char from, char to) { //func to remove edge
     for (int i = 0; i <= numVertices; i++) {
       for (int j = 0; j <= numVertices; j++) {
         if (adjMatrix[i][0] == from && adjMatrix[j][0] == to) {
-          adjMatrix[i][j] = 0;
+          adjMatrix[i][j] = ; //finding and removing
         }
       }
     }
   }
-
+  //int to build reduced graph
   int** createReducedMatrix() {
     int **reducedMatrix = new int*[numVertices - 1];
     for (int i = 0; i < numVertices - 1; i++) {
@@ -91,18 +99,19 @@ public:
     }
     return reducedMatrix;
   }
-
+  //func for path
   void dijkstra(int** matrix, int n, int start) {
+    //creating many variables
     int maxSize = numVertices - 1;
     int cost[maxSize][maxSize], distance[maxSize], pred[maxSize], visited[maxSize];
     int count, minDistance, nextNode;
-
+    //creating the cost matrix
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
         cost[i][j] = (matrix[i][j] == 0) ? 999 : matrix[i][j];
       }
     }
-
+    //creating distances predecessors and the already visited arrays
     for (int i = 0; i < n; i++) {
       distance[i] = cost[start][i];
       pred[i] = start;
@@ -111,7 +120,7 @@ public:
     distance[start] = 0;
     visited[start] = 1;
     count = 1;
-
+    //main loop for the algorithm
     while (count < n - 1) {
       minDistance = 999;
       for (int i = 0; i < n; i++) {
@@ -131,7 +140,7 @@ public:
       }
       count++;
     }
-
+    //printing
     int destination = n - 1;
     cout << "Cost: " << distance[destination] << endl;
     cout << "Path: " << (char)adjMatrix[0][destination + 1];
@@ -142,7 +151,7 @@ public:
     } while (trace != start);
     cout << endl;
   }
-
+  //func to print the matrix
   void print() {
     for (int i = 0; i < numVertices; i++) {
       for (int j = 0; j < numVertices; j++) {
@@ -156,10 +165,12 @@ public:
     }
   }
 };
-
+//main loop
 int main() {
+  //creating the graph
   Graph g;
   while (true) {
+    //getting user input and calling nessarary functions for the different inputs
     char command[10];
     char label1, label2;
     int weight;
